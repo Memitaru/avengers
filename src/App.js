316418c5ac28c01866aa80a594274d28e3939d09
Memitaru <amandatwah@gmail.com>
2019-03-26 17:07:55 -0700

    Imported avengers data and started to display it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom"; 
+
+import Home from './Components/Home';
+import AvengersList from './Components/AvengersList';
+
+import avengers from './data';
+
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      avengers: avengers
+    }
+  }
   render() {
     return (
       <div className="App">
-        
+        <Route exact path="/" component={Home} />
+        <Route path="/avengers" component={AvengersList} />
       </div>
     );
   }
